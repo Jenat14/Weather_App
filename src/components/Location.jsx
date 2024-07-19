9acbda6c4972c8a3d3888a4f653c0608497f5ab1
unknown <jenatcanil14@gmail.com>
2024-07-20 01:41:52 +0530

    Iconss

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
+import Icon from "./Icon";
 export default function Location() {
+    const Comp =({title,data}) =>(
+        <div className="p-3 bg-[#ffffff] text-[#8F43EE] shadow-lg rounded-lg">
+            <h4 className="text-center">{title}</h4>
+            <p className="text-center">{data}</p>
+        </div>
+    );
     const [lat ,setLat] = useState(null);
     const [long ,setLong] = useState(null);
     const [data, setData] = useState(null);
@@ -45,21 +52,25 @@ export default function Location() {
     return () => clearInterval(interval);
     }, [lat, long]);
 
-    
+    console.log(data)
     return (
         <div>  
-            <div className="w-[400px] bg-[#8F43EE] p-10 text-[#CBE4DE] h-[max-content] shadow-lg rounded-lg">
+            <div className="w-[400px] bg-[#8F43EE] p-10 text-[#ffffff] h-[max-content] shadow-lg rounded-lg">
                 {data ? (
                     <div>
                         <h3 className="text-center font-bold  text-2xl">{data.name}</h3>
                         <p className="text-center font-medium text-md my-2">{currentDate} ,{currentTime}</p>
+                        <Icon img={data.weather[0]}/>
                         <h2 className="text-center mt-5 text-4xl">{data.main.temp}°C</h2>
                         <p className="mt-2 text-center font-medium text-md">{data.weather[0].description}</p>
-                        <ul className="mt-5">
-                            <li className="text-center mb-2">Pressure: {data.main.pressure}</li>
-                            <li className="text-center mb-2">Humidity: {data.main.humidity}</li>
-                            <li className="text-center mb-2">Wind Speed: {data.wind.speed}</li>
-                        </ul>                   
+                        <div className="flex justify-evenly mt-4 width-[50px]">
+                            <Comp title="Pressure"
+                            data= {data.main.pressure}/>
+                            <Comp title="Humidity"
+                            data={data.main.pressure}/>
+                            <Comp title="Wind Speed"
+                            data={data.wind.speed}/>
+                        </div>                 
                     </div>  
                 ) : (
                     <div className="flex place-content-center mt-10">
