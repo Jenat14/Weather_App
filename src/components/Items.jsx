bfc7cf51d0d513ef35cd057eba6fca29ce7693d2
unknown <jenatcanil14@gmail.com>
2024-07-20 01:41:24 +0530

    removed file

--- src/components/Items.jsx
+++
@@ -1,8 +0,0 @@
-export default function Items(cont) {
-    return(
-        <div className=" h-[100px] w-[350px] bg-[#8F43EE] text-[#ffffff] shadow-lg rounded-lg">
-            <h2 className="text-center p-3 text-xl font-medium">{cont.title}</h2>
-            <p className="text-center">{cont.value}</p>
-        </div>
-    );
-} 
