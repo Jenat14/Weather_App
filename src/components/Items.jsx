6f4cf6c7d634edd1bb43f5ed99315561fa953259
unknown <jenatcanil14@gmail.com>
2024-07-20 00:20:53 +0530

    Color

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,6 +1,6 @@
 export default function Items(cont) {
     return(
-        <div className=" h-[100px] w-[400px] bg-[#2C3333] text-[#ffffff] shadow-lg rounded-lg">
+        <div className=" h-[100px] w-[350px] bg-[#8F43EE] text-[#ffffff] shadow-lg rounded-lg">
             <h2 className="text-center p-3 text-xl font-medium">{cont.title}</h2>
             <p className="text-center">{cont.value}</p>
         </div>
