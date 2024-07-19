2f0343c9b8a785e0da74f338cbdf29f0be9899a2
unknown <jenatcanil14@gmail.com>
2024-07-19 12:02:49 +0530

    items

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,8 +1,8 @@
-export default function Items(props) {
+export default function Items(cont) {
     return(
         <div className=" h-[100px] w-[400px] bg-[#2C3333] text-[#ffffff] shadow-lg rounded-lg">
-            <h2 className="text-center p-3">{props.title}</h2>
-            <p className="text-center">{props.value}</p>
+            <h2 className="text-center p-3 text-xl font-medium">{cont.title}</h2>
+            <p className="text-center">{cont.value}</p>
         </div>
     );
 } 
